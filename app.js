6b8809e136bf0020270da9fb4a915a8017000682
leonardog9823 <guanuco43@gmail.com>
2022-05-08 00:28:30 -0300

    vista corregido

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.static("public"));
+
 app.set("views", path.join("./views"));
 app.set("view engine", "pug")
 
